8c98264abf1f9416508522216231d01ab40e0c20
Alecraft57 <alemirram3@alu.edu.gva.es>
2024-12-05 20:04:53 +0100

    cambio frecuencia

--- .idea/Estructurabasica/Frecuency.java
+++ .idea/Estructurabasica/Frecuency.java
@@ -4,5 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Frequency {
+public class freq {
     public static void main(String[] args) {
+        ArrayList<String>numeros=new ArrayList<>();
+        numeros.add("uno");
+        numeros.add("dos");
+        numeros.add("dos");
+        System.out.println(NumerosCont(numeros));
+    }
+    public static Map<String,Integer>NumerosCont(ArrayList<String>numeros){
+        Map<String,Integer>nums=new HashMap<>();
+        for(String numerines:numeros){
+            nums.put(numerines,nums.getOrDefault(numerines,0)+1);
+        }
+        return nums;
+    }
+}
